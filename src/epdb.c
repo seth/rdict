46f99083c1168c5af37789abf9298b029be4559d
Seth Falcon <seth@userprimary.net>
2010-05-23 15:47:59 -0700

    Fix off-by-one bug in epdb ep_store

--- src/epdb.c
+++ src/epdb.c
@@ -105,7 +105,7 @@ int ep_store(epdb *db, SEXP s, SEXP *pv, int *i)
     } else {
         struct pvect *pvnode = db->pvect_list;
         *pv = pvnode->pvect;
-        if (pvnode->free_index < LENGTH(*pv)) {
+        if (pvnode->free_index < LENGTH(*pv) - 1) {
             *i = pvnode->free_index;
             pvnode->free_index++;
         } else {                /* need a new pvect */
